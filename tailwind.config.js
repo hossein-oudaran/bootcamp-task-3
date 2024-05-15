b27b66ae4f437ab4bf54357efd510ed79116913e
hossein-oudaran <hossein.oudaran@gmail.com>
2024-05-15 22:25:22 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}",],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Vazirmatn"],
+      },
+    },
+  },
+  plugins: [],
+};
